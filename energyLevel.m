1afcf006669031c5e28d4d5940232158e29d5124
Derek Li <lij@winchesterthurston.org>
2022-04-25 20:53:32 -0700

    symfunc notation

--- energyLevel.m
+++ energyLevel.m
@@ -1,5 +1,5 @@
 function E = energyLevel(n, ml)
-% Compute the non-dimensionalized energy eigenvalue (by h*ω_c) given the
-% specifications of an eigenfunction of energy.
+% Compute the energy eigenvalue (scaled by 1/(h*ω_c)) given the
+% quantum numbers specifying an eigenfunction of energy.
 
 E = 1/2 + n + (abs(ml)-ml)/2;
--- symmetricLandau.m
+++ symmetricLandau.m
@@ -1,4 +1,4 @@
-function [Eef, J] = symmetricLandau(n, ml, l0)
+function [Eef, J, Jp, Ja, Jc, Jpc, Jac] = symmetricLandau(n, ml, l0)
 % Compute the Landau states of a symmetric gauge for an electron given the
 % radial and angular momentum quantum numbers, with an optional specified
 % value of magnetic length.
@@ -9,7 +9,7 @@ assumeAlso(r >= 0)
 %%%%%%%% Radial dependence %%%%%%%%
 % Radial normalization.
 N = sqrt(factorial(n)/(l0^2*2^abs(ml)*factorial(n+abs(ml))));
-% Corresponding Laguerre polynomial.
+% Corresponding associated Laguerre polynomial.
 L = assocLaguerre(n, abs(ml));
 
 R(r) = N*(r/l0).^abs(ml) .* exp(-r.^2/(4*l0^2)) .* L(r.^2/(2*l0^2));
@@ -21,5 +21,5 @@ Phi(phi) = exp(1i*ml*phi) / sqrt(2*pi);
 Eef(r, phi) = simplify(R(r)*Phi(phi));
 
 % Probability current, omitting h/2m, in planar polar coordinates.
-J = pcurrent(Eef);
+[J, Jp, Ja, Jc, Jpc, Jac] = current(Eef, l0);
 
