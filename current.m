458051f247b4bc7fb2f851dc81acd3cb071677a1
Derek Li <lij@winchesterthurston.org>
2022-04-25 20:52:01 -0700

    component symbolic fxns for J

--- current.m
+++ current.m
@@ -1,8 +1,14 @@
-function J = current(wf, l0)
+function [J, Jp, Ja, Jc, Jpc, Jac] = current(wf, l0)
 % Compute the probability current of a given wave function with the
 % dimensionless constant of the magnetic length.
 
 syms r phi real
 assumeAlso(r >= 0)
-% Sum momentum and gauge currents.
-J(r,phi) = pcurrent(wf) + gcurrent(wf, l0);
+
+[Jp, Jpc] = pcurrent(wf);
+[Ja, Jac] = gcurrent(wf, l0);
+% Sum momentum and gauge currents. Vector in polar basis.
+J(r,phi) = symfun(Jp + Ja, [r,phi]);
+Jc = cell(1,2);
+Jc{1} = Jpc{1} + Jac{1};
+Jc{2} = Jpc{2} + Jac{2};
--- pcurrent.m
+++ pcurrent.m
@@ -1,12 +1,19 @@
-function J = pcurrent(wf)
+function [J, Jc] = pcurrent(wf)
 
 syms r phi t real
+assumeAlso(r >= 0)
 
-% Compute gradient of wave function.
-G = sym('G', [2 1]);
-G(1) = diff(wf, r);
-G(2) = diff(wf, phi) ./ r;
+% Compute gradient of wave function in the polar basis.
+Gr = diff(wf, r);
+Gphi = diff(wf, phi) ./ r;
 
-% Probability current, omitting h/2m, in planar polar coordinates.
-J(r, phi) = conj(wf).*G - wf.*conj(G);
-J = simplify(-1i*J);
+% Component-wise (polar) operation so the components may be separately stored.
+% Omitting h/2m.
+Jr = symfun(-1i*(conj(wf).*Gr - wf.*conj(Gr)), [r,phi]);
+Jphi = symfun(-1i*(conj(wf).*Gphi - wf.*conj(Gphi)), [r,phi]);
+
+% Produce a vector function, in the polar basis, perhaps useful for
+% symbolic operations.
+J(r,phi) = [Jr; Jphi];
+% Cell array for conversion to numeric functions for efficient evaluation.
+Jc = {Jr, Jphi};
