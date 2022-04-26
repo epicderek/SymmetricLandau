77746023bb55a2c731dbd9e794e9cf3f82d74a03
Derek Li <lij@winchesterthurston.org>
2022-04-25 21:17:51 -0700

    removed older symbolic evals

--- currents/plotCurrent.m
+++ currents/plotCurrent.m
@@ -22,20 +22,6 @@ y = rf.*sinphi;
 Jf = zeros([2 size(rf)]);
 Jf(1,:,:) = Jr(rf, phif);
 Jf(2,:,:) = Jphi(rf, phif);
-% % Remove singularity at r = 0.
-% try
-%     Jff = J(rf, phif);
-%     Jf(1,:,:) = eval(Jff{1});
-%     Jf(2,:,:) = eval(Jff{2});
-% catch
-%     warning('Singularity encountered: assumed at r = 0!')
-%     % Remove r = 0 from plotting region of current.
-%     rf2 = rf(:,2:end);
-%     phif2 = phif(:,2:end);
-%     Jff = J(rf2, phif2);
-%     Jf(1,:,2:end) = eval(Jff{1});
-%     Jf(2,:,2:end) = eval(Jff{2});
-% end
 
 % Conversion from polar to Cartesian basis.
 Jfx = squeeze(Jf(1,:,:)).*cosphi;
