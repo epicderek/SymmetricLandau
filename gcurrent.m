d06a55ed6e18268021638d713b773a9eed237e2a
Derek Li <lij@winchesterthurston.org>
2022-04-25 20:50:38 -0700

    adding current due to symmetric gauge

--- gcurrent.m
+++ gcurrent.m
@@ -1,10 +1,15 @@
-function J = gcurrent(wf, l0)
+function [J, Jc] = gcurrent(wf, l0)
+% Gauge current of a symmetric gauge.
 
 syms r phi real
 assumeAlso(r >= 0)
-J = sym('J', [2 1]);
-J(1) = 0;
-% Omitting h/2m.
-J(2) = -1/l0^2 * r .* wf(r,phi).*conj(wf(r,phi));
 
-J(r,phi) = J;
+% Current in polar basis.
+Jr = symfun(0, [r,phi]);
+% Omitting h/2m.
+Jphi(r,phi) = symfun(-1/l0^2 * r .* wf.*conj(wf), [r,phi]);
+
+% Vector function.
+J(r,phi) = [Jr; Jphi];
+% Cell array for conversion to numeric functions for efficient evaluation.
+Jc = {Jr, Jphi};
